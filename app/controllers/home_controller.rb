813946d5058829bbefa9639c3041206ad3ed85a5
Kristian <nesto@a.a>
2014-07-02 21:37:05 +0200

    sve uredjeno i pocetak prijevoda napravljen

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,23 @@
 class HomeController < ApplicationController
   def pocetna
+    
   end
 
   def onama
+   
   end
 
   def uslugeisavjeti
+    flash[:notice] = t(:intro)
   end
 
   def galerija
+
   end
 
   def kontakt
+    
   end
+
 end
-
-
 
